93e7a851cfac6b56ae0730c107e55258f17cfb89
kalyanipatil <kalyanipatil1294@gmail.com>
2026-02-18 17:57:57 -0800

    done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Remove the '1' here!
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kalyani-portfolio/", 
+  base: '/kalyani-portfolio/', 
 })
